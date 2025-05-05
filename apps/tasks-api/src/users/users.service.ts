774a6521f016a495ac778d5f78d07268453dc5c7
fagneraureliano <fagner2aureliano@gmail.com>
2025-05-04 22:11:17 -0300

    feat: :sparkles: impl config prisma to database and insert first register

--- apps/tasks-api/src/users/users.service.ts
+++ apps/tasks-api/src/users/users.service.ts
@@ -1,11 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/database/prisma.service';
+import { hash } from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+
+  constructor(private prisma: PrismaService) { }
+  async create(createUserDto: CreateUserDto) {
+    const encryptedPassword = await hash(
+      createUserDto.password,
+      10,
+    );
+
+    const userExists = await this.prisma.user.findUnique({
+      where: {
+        email: createUserDto.email,
+      },
+    });
+    if (userExists) {
+      throw new Error('User already exists');
+    }
+    const user = await this.prisma.user.create({
+      data: {
+        ...createUserDto,
+        password: encryptedPassword,
+      },
+    });
+    return user;
   }
 
   findAll() {
