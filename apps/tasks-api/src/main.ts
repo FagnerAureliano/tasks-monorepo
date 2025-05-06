cbe56f911a3c26f0168bc8264f956e8bba218de6
fagneraureliano <fagner2aureliano@gmail.com>
2025-05-05 21:13:40 -0300

    feat: :sparkles: impl validation and swagger

--- apps/tasks-api/src/main.ts
+++ apps/tasks-api/src/main.ts
@@ -1,8 +1,20 @@
+
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app: any = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('CRUD Tasks API')
+    .setDescription('The tasks API description')
+    .setVersion('1.0')
+    .addTag('tasks')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
