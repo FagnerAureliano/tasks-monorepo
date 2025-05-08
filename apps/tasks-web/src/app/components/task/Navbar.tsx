aec68af442053cc40ea801dd8eae1595c73c5ced
fagneraureliano <fagner2aureliano@gmail.com>
2025-05-08 14:50:49 -0300

    feat: :construction: create navbar component

---
+++ apps/tasks-web/src/app/components/task/Navbar.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useLogout } from "@/app/lib/auth/useLogout";
+
+type NavbarProps = {
+  userName: string;
+};
+
+export default function Navbar({ userName }: NavbarProps) {
+  const logout = useLogout();
+
+  return (
+    <nav className="bg-auto text-white p-4 flex justify-between items-center shadow-md">
+      <div className="text-xl font-semibold">Hello, {userName}!</div>
+      <button
+        className="flex cursor-pointer items-center gap-2 bg-green-500 hover:bg-green-600 text-white font-medium py-[4px] pl-3 pr-4 rounded-lg transition-all duration-200 transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-50"
+        onClick={logout}
+        aria-label="Logout dsystem"
+      >
+        <svg
+          className="w-5 h-5 text-gray-800 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="22"
+          height="22"
+          fill="none"
+          viewBox="0 0 22 22"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M20 12H8m12 0-4 4m4-4-4-4M9 4H7a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h2"
+          />
+        </svg>
+
+        <span>Exit</span>
+      </button>
+    </nav>
+  );
+}
